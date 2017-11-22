2cd34da56ba63248e29f75a5d68119a1e553669e
Maswor <maswortxt@gmail.com>
2017-11-22 13:11:15 -0600

    Right before experiment with sampling

--- Point-Cloud_Procesing_1_0/point_cloud_1_0_source_code/Point Cloud/src/DataMgr.h
+++ Point-Cloud_Procesing_1_0/point_cloud_1_0_source_code/Point Cloud/src/DataMgr.h
@@ -1,72 +1,65 @@
 #pragma once
-#include "cmesh.h"
-#include "Parameter.h"
-#include "GlobalFunction.h"
 #include "Algorithm/Skeleton.h"
+#include "GlobalFunction.h"
+#include "Parameter.h"
+#include "cmesh.h"
 
-
-
-#include <wrap/io_trimesh/import.h>
 #include <wrap/io_trimesh/export.h>
+#include <wrap/io_trimesh/import.h>
 
-#include <sstream>
 #include <fstream>
 #include <set>
-
+#include <sstream>
 
 using namespace vcg;
 using namespace std;
 using namespace tri;
 
-
-class DataMgr
-{
+class DataMgr {
 public:
-	DataMgr(RichParameterSet* _para);
-	~DataMgr(void);
+    DataMgr(RichParameterSet* _para);
+    ~DataMgr(void);
 
-	void loadPlyToOriginal(QString fileName);
-	void loadPlyToSample(QString fileName);
-	void savePly(QString fileName, CMesh& mesh);
-	void loadImage(QString fileName);
-  void loadXYZN(QString fileName);
+    void loadPlyToOriginal(QString fileName);
+    void loadPlyToSample(QString fileName);
+    void savePly(QString fileName, CMesh& mesh);
+    void loadImage(QString fileName);
+    void loadXYZN(QString fileName);
 
-	bool isSamplesEmpty();
-	bool isOriginalEmpty();
-  bool isSkeletonEmpty();
+    bool isSamplesEmpty();
+    bool isOriginalEmpty();
+    bool isSkeletonEmpty();
 
-	CMesh* getCurrentSamples();
-	CMesh* getCurrentOriginal();
-	Skeleton* getCurrentSkeleton();
+    CMesh* getCurrentSamples();
+    CMesh* getCurrentOriginal();
+    Skeleton* getCurrentSkeleton();
 
-	void recomputeBox();
-	double getInitRadiuse();
+    void recomputeBox();
+    double getInitRadiuse();
 
-	void downSamplesByNum(bool use_random_downsample = true);
-	void subSamples();
+    void downSamplesByNum(bool use_quasi_random_downsample = true);
+    void subSamples();
 
-	void normalizeROSA_Mesh(CMesh& mesh);
-	Box3f normalizeAllMesh();
+    void normalizeROSA_Mesh(CMesh& mesh);
+    Box3f normalizeAllMesh();
 
-	void eraseRemovedSamples();
-	void clearData();
-	void recomputeQuad();
+    void eraseRemovedSamples();
+    void clearData();
+    void recomputeQuad();
 
-	void loadSkeletonFromSkel(QString fileName);
-	void saveSkeletonAsSkel(QString fileName);
-
+    void loadSkeletonFromSkel(QString fileName);
+    void saveSkeletonAsSkel(QString fileName);
 
 private:
-	void clearCMesh(CMesh& mesh);
+    void clearCMesh(CMesh& mesh);
 
 public:
-	CMesh original;
-	CMesh samples;
-	Skeleton skeleton;
-	//cv::Mat image;
+    CMesh original;
+    CMesh samples;
+    Skeleton skeleton;
+    //cv::Mat image;
 
-	RichParameterSet* para;
-	double init_radius;
-	QString curr_file_name;
+    RichParameterSet* para;
+    double init_radius;
+    QString curr_file_name;
 };
-
